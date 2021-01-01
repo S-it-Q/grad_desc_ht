aad05e84a04e5268dc0fba26b84d0b74d882d3e4
Etheon <snow_chris@naver.com>
2021-01-01 23:50:02 +0900

    Project Setup with Clion

---
+++ cmake-build-debug/CMakeFiles/grad_desc_ht.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grad_desc_ht.dir/src/main.cpp.obj"
+  "grad_desc_ht.exe"
+  "grad_desc_ht.exe.manifest"
+  "grad_desc_ht.pdb"
+  "libgrad_desc_ht.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grad_desc_ht.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
